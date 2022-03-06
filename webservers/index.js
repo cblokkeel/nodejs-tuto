608c065633b6c59f15134d78d31dad3baabe45b0
Colin Blokkeel <cblokkeeltech@gmail.com>
2022-03-06 18:34:18 +0100

    first webserver

---
+++ webservers/index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    })
+
+    res.end(JSON.stringify({
+        id: 1,
+        name: 'test'
+    }))
+})
+
+server.listen(PORT, () => console.log(`Listening on port ${3000}`))
