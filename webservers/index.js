e10be09644eb0263ef7a488f482c302913607094
Colin Blokkeel <cblokkeeltech@gmail.com>
2022-03-06 18:47:07 +0100

    routing

--- webservers/index.js
+++ webservers/index.js
@@ -2,15 +2,46 @@ const http = require('http')
 
 const PORT = 3000
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    })
-
-    res.end(JSON.stringify({
+const friends = [
+    {
+        id: 0,
+        name: 'Nikola Tesla'
+    },
+    {
         id: 1,
-        name: 'test'
-    }))
+        name: 'Albert Einstein'
+    },
+    {
+        id: 2,
+        name: 'Isaac Newton'
+    },
+]
+
+const server = http.createServer((req, res) => {
+    const items = req.url.split('/')
+
+    if (items[1] === 'friends') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) 
+            res.end(JSON.stringify(friends[+items[2]]))
+        else 
+            res.end(JSON.stringify(friends))
+    } else if (items[1] === 'messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello Isaac!</li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.statusCode = 404
+        res.end()
+    }
 })
 
 server.listen(PORT, () => console.log(`Listening on port ${3000}`))
