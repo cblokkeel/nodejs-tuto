4e6da630ae19ef59da74f07e7b829a08cae41262
Colin Blokkeel <cblokkeeltech@gmail.com>
2022-03-07 19:54:15 +0100

    post method

--- express/server.js
+++ express/server.js
@@ -21,6 +21,23 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.url} ${delta}ms`)
 })
 
+app.use(express.json())
+
+app.post('/friends', (req, res) => {
+    if (!req.body.name) {
+       return res.status(400).json({error: 'data incorrect (missing friends name)'})
+    } 
+
+    const newFriend = {
+        id: friends.length,
+        name: req.body.name,
+    }
+
+    friends.push(newFriend)
+
+    res.json(newFriend)
+})
+
 app.get('/', (req, res) => {
     res.send('hello world')
 })
