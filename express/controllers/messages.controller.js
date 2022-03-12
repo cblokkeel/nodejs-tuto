9da157a022954ec40196425f2fda863715ff5623
Colin Blokkeel <cblokkeeltech@gmail.com>
2022-03-12 11:24:00 +0100

    express fini

--- express/controllers/messages.controller.js
+++ express/controllers/messages.controller.js
@@ -2,7 +2,11 @@ const path = require('path')
 
 function getMessages (req, res) {
     // res.send('<ul><li>hello</li></ul>')
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    res.render('messages', {
+        title: 'Super message',
+        friend: 'Elon musk'
+    })
 }
 
 function postMessage (req, res) {
